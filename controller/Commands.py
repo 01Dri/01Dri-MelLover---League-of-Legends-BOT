2e327ed1f6a16dbc6d2557f988af567a2f2ab94d
01Dri <drikill01@gmaiil.com>
2024-01-19 20:29:08 -0300

    Some files organized

--- controller/Commands.py
+++ controller/Commands.py
@@ -18,13 +18,20 @@ class BotCommands:
         content_message = ctx.content.lower()
         self.guid_musics[ctx.author.id] = Player(DEFAULT_PATH_FOLDER_DOWNLOAD, ctx, None)
         if content_message.startswith("!contalol"):
-            lol_services = LolServices(ctx)
-            inicio = time.time()
-            await lol_services.get_account_lol_info(ctx)
-            fim = time.time()
-            tempo_execucao = fim - inicio
-            print(f"A função levou {tempo_execucao} segundos para executar.")
+            await self.handler_lol_srvices(ctx)
 
+        if content_message.startswith("!m"):
+            await self.handler_player_music(ctx, content_message)
+
+    async def handler_lol_srvices(self, ctx):
+        lol_services = LolServices(ctx)
+        inicio = time.time()
+        await lol_services.get_account_lol_info(ctx)
+        fim = time.time()
+        tempo_execucao = fim - inicio
+        print(f"A função levou {tempo_execucao} segundos para executar.")
+
+    async def handler_player_music(self, ctx, content_message):
         if content_message.startswith("!mplay"):
             message_music = ctx.content.split()
             url_music = message_music[1]
@@ -39,15 +46,3 @@ class BotCommands:
             else:
                 print(self.guid_musics)
                 print("nao")
-            ##  await youtube_download.add_music_queue_to_download()
-
-            # Run download and play tasks concurrently
-            ## download_task = asyncio.create_task(youtube_download.download_musics_on_queue())
-            ## await asyncio.gather(download_task, self.play_music(ctx))
-
-##  async def play_music(self, ctx):
-##    player = Player(
-##      f"C:\\Users\\didvg\\Desktop\\DevFolders\\MelLover2.0folder_for_downloads_musics\\{ctx.guild.id}",
-##   ctx)
-##   await player.connect_bot()
-##  await player.play()
