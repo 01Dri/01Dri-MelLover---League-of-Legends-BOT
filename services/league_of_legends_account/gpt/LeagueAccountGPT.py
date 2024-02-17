06357dc111fe017b6bc77c494f56920c88a9275c
01Dri <drikill01@gmail.com>
2024-02-17 20:52:46 -0300

    kkkkk fiz cagada

--- services/league_of_legends_account/gpt/LeagueAccountGPT.py
+++ services/league_of_legends_account/gpt/LeagueAccountGPT.py
@@ -1,15 +1,21 @@
+from dotenv import load_dotenv
 from openai import OpenAI
 
 from entities.entities_league_of_legends_account.AccountLoL import AccountLoL
+import os
 
 
 class LeagueAccountGPT:
 
     def __init__(self):
-        self.client = OpenAI(api_key="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")
+        self.client = OpenAI()
+        load_dotenv()
+        self.TOKEN_GPT = os.getenv("OPENAI_TOKEN")
+
         pass
 
     def get_tips(self, account_instance: AccountLoL):
+        self.client.api_key = self.TOKEN_GPT
         response = self.client.completions.create(
             model="gpt-3.5-turbo-instruct",
             max_tokens=200,
@@ -20,4 +26,3 @@ class LeagueAccountGPT:
                    f"{account_instance.league} League"
                    f"{account_instance.winrate} Winrate")
         return response.choices[0].text
-
